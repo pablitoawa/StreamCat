299e21b1bf8c24742a16761444b5e90ac486cc7e
pablitoawa <pablo77080302@hotmail.com>
2024-06-18 15:25:40 -0500

    productosScreen

--- lib/screens/ProductosScreen.dart
+++ lib/screens/ProductosScreen.dart
@@ -1,12 +1,13 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const Productos());
 }
 
 class Productos extends StatelessWidget {
-  const Productos({Key? key}) : super(key: key);
+  const Productos({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,14 +19,15 @@ class Productos extends StatelessWidget {
 }
 
 class Lista extends StatefulWidget {
-  const Lista({Key? key}) : super(key: key);
+  const Lista({super.key});
 
   @override
   State<Lista> createState() => _ListaState();
 }
 
 class _ListaState extends State<Lista> {
-  List<Map<dynamic, dynamic>> productList = [];
+  List<Map<dynamic, dynamic>> movieList = [];
+  String userName = "Juan"; // Reemplaza con el nombre real del usuario
 
   @override
   void initState() {
@@ -34,23 +36,27 @@ class _ListaState extends State<Lista> {
   }
 
   void getData() {
-    DatabaseReference productoRef = FirebaseDatabase.instance.ref('producto');
-    productoRef.onValue.listen((DatabaseEvent event) {
+    DatabaseReference movieRef = FirebaseDatabase.instance.ref('movies');
+    movieRef.onValue.listen((DatabaseEvent event) {
       final data = event.snapshot.value;
-      updateProductList(data);
+      updateMovieList(data);
     });
   }
 
-  void updateProductList(dynamic data) {
+  void updateMovieList(dynamic data) {
     if (data != null) {
       List<Map<dynamic, dynamic>> tempList = [];
       data.forEach((key, value) {
-        tempList
-            .add({"producto": value['producto'], "precio": value['precio']});
+        tempList.add({
+          "title": value['title'],
+          "poster": value['poster'],
+          "description": value['description'],
+          "rating": value['rating']
+        });
       });
 
       setState(() {
-        productList = tempList;
+        movieList = tempList;
       });
     }
   }
@@ -58,16 +64,125 @@ class _ListaState extends State<Lista> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Firebase ListView'),
+      backgroundColor: Colors.grey[900],
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                "Bienvenido de vuelta, $userName",
+                style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: movieList.length,
+                itemBuilder: (context, index) {
+                  return MovieCard(
+                    title: movieList[index]["title"],
+                    poster: movieList[index]["poster"],
+                    description: movieList[index]["description"],
+                    rating: movieList[index]["rating"],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
-      body: ListView.builder(
-        itemCount: productList.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('${productList[index]["producto"]}'),
-          );
-        },
+    );
+  }
+}
+
+class MovieCard extends StatelessWidget {
+  final String title;
+  final String poster;
+  final String description;
+  final double rating;
+
+  const MovieCard({
+    super.key,
+    required this.title,
+    required this.poster,
+    required this.description,
+    required this.rating,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.grey[800],
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(16.0),
+              bottomLeft: Radius.circular(16.0),
+            ),
+            child: Image.network(
+              poster,
+              width: 120,
+              height: 180,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 8.0),
+                  Text(
+                    description,
+                    style: GoogleFonts.poppins(
+                      color: Colors.grey[400],
+                      fontSize: 14,
+                    ),
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(height: 8.0),
+                  Row(
+                    children: [
+                      const Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                        size: 18,
+                      ),
+                      const SizedBox(width: 4.0),
+                      Text(
+                        rating.toString(),
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
